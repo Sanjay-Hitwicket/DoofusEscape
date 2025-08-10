2fc863a14f6d5948e952c42bace9260d6c7e8f2d
Sanjay-Hitwicket <sanjay@hitwicket.com>
2025-08-11 00:07:54 +0530

    feat: so for tile map references

---
+++ Assets/Scripts/SO/TilesLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SO {
+    [CreateAssetMenu(fileName = "TilesLibrary", menuName = "SO/TilesLibrary")]
+    public sealed class TilesLibrary : ScriptableObject {
+        [SerializeField] private List<TileReference> tileReferences;
+        
+        public TileReference GetTileReference(TileName tileName) {
+            foreach (var tileRef in tileReferences) {
+                if (tileRef.tileType == tileName) {
+                    return tileRef;
+                }
+            }
+            return null;
+        }
+    }
+    
+    [Serializable]
+    public class TileReference {
+        public TileName tileType;
+        public GameObject tilePrefab;
+    }
+
+    public enum TileName {
+        Grass,
+        Water,
+        Sand,
+        Rock
+    }
+}
