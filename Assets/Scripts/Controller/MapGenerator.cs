d3f330381a94a9e526c642fe8cfdfb745cb1b2b9
Sanjay-Hitwicket <sanjay@hitwicket.com>
2025-08-11 00:07:12 +0530

    feat: simple map generator for map generation with tiles

---
+++ Assets/Scripts/Controller/MapGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Systems.Lightweight_DI;
+using UnityEngine;
+
+namespace DoofusEscape {
+    public class MapGenerator : BaseController{
+        public override void Initialize() {
+            
+        }
+
+        public void GetMapPosition(Vector3 position) {
+            // Logic to get map position based on the provided Vector3 position
+            Debug.Log($"Map position requested for: {position}");
+            
+        }
+    }
+}
